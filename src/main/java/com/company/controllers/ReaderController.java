ef15455db922a3a9a1d2808c8463da8d1847b827
Sebastian Lachut <seb.lachut@gmail.com>
2024-04-22 19:46:53 +0200

    Created reader REST controller and reader service class

---
+++ src/main/java/com/company/controllers/ReaderController.java
@@ -0,0 +1,38 @@
+package com.company.controllers;
+
+import com.company.dtos.ReaderDto;
+import com.company.entities.Reader;
+import com.company.mappers.ReaderMapper;
+import com.company.repositories.ReaderRepository;
+import com.company.services.ReaderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("library/v1/reader")
+public class ReaderController {
+
+    private final ReaderRepository readerRepository;
+    private final ReaderMapper readerMapper;
+    private final ReaderService readerService;
+
+    public ReaderController(ReaderRepository readerRepository, ReaderMapper readerMapper, ReaderService readerService) {
+        this.readerRepository = readerRepository;
+        this.readerMapper = readerMapper;
+        this.readerService = readerService;
+    }
+
+    @GetMapping
+    public List<ReaderDto> getAllReaders() {
+        List<Reader> readers = readerRepository.findAll();
+        return readerMapper.mapToReaderDtoList(readers);
+    }
+
+    @PostMapping
+    public ReaderDto createReader(@RequestBody ReaderDto readerDto) {
+        readerService.createReader(readerDto);
+        return readerDto;
+    }
+}
